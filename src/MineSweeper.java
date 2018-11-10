e38066b2aa0973c678f2e9425343f33138f8fda2
Keryit <kery85@ukr.net>
2018-11-10 17:03:53 +0200

    modif

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -47,6 +47,7 @@ public class MineSweeper extends JFrame {
     }
 
     private void initFrame() {
+        //comment
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Mine Sweeper");
